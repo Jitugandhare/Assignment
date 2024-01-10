90cf561c8690f611fff3a08c2cfa354e1d20d5a9
Jitu gandhare <jitugandhare@gmail.com>
2024-01-10 16:32:32 +0530

    timeline

--- peters/src/components/Timeline.jsx
+++ peters/src/components/Timeline.jsx
@@ -1,14 +1,41 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+
 
 
 const Timeline = () => {
-  return (
-    <div className='timeline'>
-      <select>
-        <option value="Jan20-Dec21">Show Timeline: <span className='date'>Jan'22-Dec'23</span></option>
-      </select>
-    </div>
-  )
+    return (
+        <TimelineWrapper>
+            <SelectWrapper>
+                <option value="Jan20-Dec21">Show Timeline: <span className='date'>Jan'22-Dec'23</span></option>
+            </SelectWrapper>
+        </TimelineWrapper>
+    );
 }
 
-export default Timeline
+export default Timeline;
+
+
+
+
+const TimelineWrapper = styled.div`
+  padding: 0 2rem;
+  margin-top: 2rem;
+  display: flex;
+  justify-content: flex-end;
+
+  @media screen and (max-width: 800px) {
+    margin-top: 0.5rem;
+    padding: 1rem;
+    justify-content: center;
+  }
+`;
+
+const SelectWrapper = styled.select`
+  width:250px;
+  padding: 10px;
+  height: 3rem;
+  border: 1px solid green;
+  border-radius: 10px;
+  color: green;
+`;
