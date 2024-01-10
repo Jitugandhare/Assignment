65b5c198518ef491e79e7d277e8b1f5ac4ac4f11
Jitu gandhare <jitugandhare@gmail.com>
2024-01-10 10:52:05 +0530

    sidebar functionality

--- peters/src/components/Sidebar.jsx
+++ peters/src/components/Sidebar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-
+import image from "../utils/img1.svg";
 
 
 
@@ -8,7 +8,7 @@ const Sidebar = () => {
     return (
         <SidebarContainer>
             <LogoContainer>
-                {/* <img src={image} alt="Logo" /> */}
+                <img src={image} alt="Logo" />
             </LogoContainer>
             <SectionContainer>
                 <SectionContent>
@@ -16,14 +16,23 @@ const Sidebar = () => {
                         <p>Data-in</p>
                     </SectionTitle>
                     <StyledSelectContainer>
-                      
-                            
-                       
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="17"
+                            height="17"
+                            viewBox="0 0 17 17"
+                            fill="none"
+                        >
+                            <path
+                                d="M12 3.76999L11.25 4.60999C11.25 4.60999 9.97 6.05999 8.68 7.93999C7.39 9.81999 6 12.07 6 14.23C6 15.8213 6.63214 17.3474 7.75736 18.4726C8.88258 19.5978 10.4087 20.23 12 20.23C13.5913 20.23 15.1174 19.5978 16.2426 18.4726C17.3679 17.3474 18 15.8213 18 14.23C18 12.07 16.61 9.81999 15.32 7.93999C14.5243 6.78422 13.6664 5.67258 12.75 4.60999L12 3.76999ZM12 6.89999C12.44 7.41999 12.84 7.84999 13.68 9.06999C14.89 10.83 16 13.07 16 14.23C16 16.45 14.22 18.23 12 18.23C9.78 18.23 8 16.45 8 14.23C8 13.07 9.11 10.83 10.32 9.06999C11.16 7.84999 11.56 7.41999 12 6.89999Z"
+                                fill="white"
+                            />
+                        </svg>
                         <StyledSelect>
                             <option>Energy</option>
                         </StyledSelect>
                     </StyledSelectContainer>
-                   
+
                 </SectionContent>
             </SectionContainer>
             <ButtonContainer>
@@ -32,7 +41,12 @@ const Sidebar = () => {
             <PoweredByContainer>
                 <PoweredByText>Powered by</PoweredByText>
                 <PoweredByLogo>
-                  
+                    <svg xmlns="http://www.w3.org/2000/svg" width="117" height="26" viewBox="0 0 117 26" fill="none">
+                        <path
+                            d="M12 3.76999L11.25 4.60999C11.25 4.60999 9.97 6.05999 8.68 7.93999C7.39 9.81999 6 12.07 6 14.23C6 15.8213 6.63214 17.3474 7.75736 18.4726C8.88258 19.5978 10.4087 20.23 12 20.23C13.5913 20.23 15.1174 19.5978 16.2426 18.4726C17.3679 17.3474 18 15.8213 18 14.23C18 12.07 16.61 9.81999 15.32 7.93999C14.5243 6.78422 13.6664 5.67258 12.75 4.60999L12 3.76999ZM12 6.89999C12.44 7.41999 12.84 7.84999 13.68 9.06999C14.89 10.83 16 13.07 16 14.23C16 16.45 14.22 18.23 12 18.23C9.78 18.23 8 16.45 8 14.23C8 13.07 9.11 10.83 10.32 9.06999C11.16 7.84999 11.56 7.41999 12 6.89999Z"
+                            fill="white"
+                        />
+                    </svg>
                 </PoweredByLogo>
             </PoweredByContainer>
         </SidebarContainer>
@@ -53,7 +67,13 @@ const SidebarContainer = styled.div`
 `;
 
 const LogoContainer = styled.div`
-  padding: 15px;
+width: Fixed (219px)
+height: Hug (73px)
+padding: 13px, 10px, 13px, 20px
+border-radius: 45px
+gap: 5px
+
+  
 
   img {
     text-align: left;
